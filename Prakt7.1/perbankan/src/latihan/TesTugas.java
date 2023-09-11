91751ed1ccebd668552e5a428780d031a2900941
illufoxKusanagi <ariefsatria2409@gmail.com>
2023-09-12 05:03:28 +0700

    coba

--- Prakt7.1/perbankan/src/latihan/TesTugas.java
+++ Prakt7.1/perbankan/src/latihan/TesTugas.java
@@ -1,30 +1,32 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package tugas2;
+package latihan;
 
 import perbankan.*;
 
 /**
  *
- * @author orang biasa
+ * @author Lenovo
  */
-public class Tugas2 {
+public class TesTugas {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Bank bank = new Bank();
+        
         bank.tambahNasabah("Agus", "Daryanto");
         bank.getNasabah(0).setTabungan(new Tabungan(5000));
+        
         bank.tambahNasabah("Tuti", "Irawan");
         bank.getNasabah(1).setTabungan(new Tabungan(7000));
+        
         bank.tambahNasabah("Ani", "Ratna");
         bank.getNasabah(2).setTabungan(new Tabungan(4000));
+        
         bank.tambahNasabah("Bambang", "Darmawan");
         bank.getNasabah(3).setTabungan(new Tabungan(6500));
+        
         System.out.println("Jumlah Nasabah = " + bank.getJumlahNasabah());
         for (int i = 0; i < bank.getJumlahNasabah(); i++) {
             System.out.println("Nasabah ke-" + (i + 1) + " : "
--- Prakt7.1/perbankan/src/perbankan/Bank.java
+++ Prakt7.1/perbankan/src/perbankan/Bank.java
@@ -2,24 +2,33 @@ package perbankan;
 
 public class Bank {
     private Nasabah[] nasabah;
-    private int jumlahNasabah;
-
-    public Bank() {
+    private int jumlahNasabah = 0;
+    
+    public Bank(){
+        nasabah = new Nasabah[100];
     };
-
-    public void tambahNasabah(String namaAwal, String namaAkhir) {
-        if (nasabah == null) {
-            nasabah = new Nasabah[100];
-        }
+    
+    public void tambahNasabah(String namaAwal, String namaAkhir){
         nasabah[jumlahNasabah] = new Nasabah(namaAwal, namaAkhir);
         jumlahNasabah++;
     }
-
-    public int getJumlahNasabah() {
+    
+    public int getJumlahNasabah(){
         return jumlahNasabah;
     }
-
-    public Nasabah getNasabah(int indeks) {
+    
+    public Nasabah getNasabah(int indeks){
         return nasabah[indeks];
+    }
+    
+    public int getIndexNasabah(String namaAwal, String namaAkhir) {
+        int index = 0;
+        for(Nasabah nasabah :this.nasabah) {
+            if(nasabah.getNamaAwal().equals(namaAwal) && nasabah.getNamaAkhir().equals(namaAkhir)) {
+                return index;
+            }
+            index++;
+        }
+        return 0;
     }
 }
